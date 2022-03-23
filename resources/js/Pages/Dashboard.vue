9d266759256ee8c9b8cf5841ac7bde080323cae3
fredrick rogath <fredlee560@gmail.com>
2022-03-23 09:43:42 +0300

    first cimmit

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,44 @@
+<template>
+  <app-layout title="Dashboard">
+
+    <div class="py-4">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div
+          class="overflow-hidden shadow-xl sm:rounded-lg overflow-auto"
+        >
+          <div class="flex flex-wrap justify-evenly my-2">
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+            <image-card></image-card>
+          
+          </div>
+        </div>
+      </div>
+    </div>
+  </app-layout>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import AppLayout from "@/Layouts/AppLayout.vue";
+import Welcome from "@/Jetstream/Welcome.vue";
+import ImageCard from "@/Jetstream/ImageCard.vue";
+
+export default defineComponent({
+  components: {
+    AppLayout,
+    Welcome,
+    ImageCard,
+  },
+});
+</script>
